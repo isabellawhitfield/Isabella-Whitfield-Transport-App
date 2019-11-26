6406f4696a910088f9719c969f22056b46ffe9f1
isabellawhitfield <270026136@bun2-206-08.acg.ac.nz>
2019-11-26 15:18:24 +1300

    added shell. Added motorcycle objects.

---
+++ js/script.js
@@ -0,0 +1,91 @@
+
+
+
+
+// Storing the cars as objects, so they can be made into cards and modals
+// and there information can be searched by arrays for final screen calculations
+var carsAvailable = [
+    {
+        name: "BMW F700 GS",
+        type: "Motorbike",
+        photo1: "images/motorbikeBMW1.jpg",
+        photo2: "images/motorbikeBMW2.jpg",
+        photo3: "images/motorbikeBMW3.jpg",
+        fuelType: "Premium Unleaded 95",
+        fuelEfficency: 3.71,
+        transmission: "Manual",
+        minPeople: 1,
+        maxPeople: 1,
+        minDays: 1,
+        maxDays: 5,
+        price: 109,
+        manufacturedDate: "2016",
+        engine: "798cc four stroke 75hp",
+        carDescription: "The ride with BMW F 700 GS is not just safer but also offers especially high comfort – low weight, reduced seat height and a " +
+        "lowering option. Ready for carefree, fun and comfortable motorcycling? Especially the BMW F 700 GS’ 75bhp engine and its outstanding handling" + 
+        " riding becomes child’s play. Due to its low weight and many different seat variants from high to low-slung, it is also particularly suited to " + 
+        "female riders and beginners as an ideal all-round endro machine."
+
+
+    },
+    {
+        name: "Suzuki SV 650",
+        type: "Motorbike",
+        photo1:
+        photo2:
+        photo3:
+        fuelType:
+        fuelEfficency:
+        transmission:
+        minPeople:
+        maxPeople:
+        minDays:
+        maxDays:
+        price:
+        carDescription:
+        manufacturedDate:
+
+
+
+    },
+    {
+        name:
+        type:
+        photo1:
+        photo2:
+        photo3:
+        fuelType:
+        fuelEfficency:
+        transmission:
+        minPeople:
+        maxPeople:
+        minDays:
+        maxDays:
+        price:
+        carDescription:
+        manufacturedDate:
+
+
+
+    },
+    {
+        name:
+        type:
+        photo1:
+        photo2:
+        photo3:
+        fuelType:
+        fuelEfficency:
+        transmission:
+        minPeople:
+        maxPeople:
+        minDays:
+        maxDays:
+        price:
+        carDescription:
+        manufacturedDate:
+
+
+
+    },
+]
